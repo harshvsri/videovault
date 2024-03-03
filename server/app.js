12b3e5e6da399bb18fcd4f3258dad39a143814fe
Harsh V Srivastava <harshvsri@gmail.com>
2024-03-03 12:30:56 +0530

    Remove unnecessary files.

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ var uploadRouter = require("./routes/upload.router");
 var connectDB = require("./configs/db.config");
 var corsOptions = require("./configs/cors.config");
 var sessionOptions = require("./configs/session.config");
+const { error } = require("console");
 require("./configs/passport.config");
 
 var app = express();
@@ -54,12 +55,12 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // res.locals.message = err.message;
+  const errorDetails = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json(errorDetails);
 });
 
 module.exports = app;
