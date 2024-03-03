deaee0c4aacc032dc833ff928326a261fd66218e
Harsh V Srivastava <harshvsri@gmail.com>
2024-03-03 12:03:49 +0530

    Add authentication feature

--- server/app.js
+++ server/app.js
@@ -6,12 +6,15 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 var session = require("express-session");
+var passport = require("passport");
 
+var indexRouter = require("./routes/index.router");
 var userRouter = require("./routes/user.router");
 var uploadRouter = require("./routes/upload.router");
 var connectDB = require("./configs/db.config");
 var corsOptions = require("./configs/cors.config");
 var sessionOptions = require("./configs/session.config");
+require("./configs/passport.config");
 
 var app = express();
 
@@ -22,6 +25,15 @@ app.set("view engine", "ejs");
 connectDB();
 app.use(session(sessionOptions));
 app.use(cors(corsOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// debug session and user
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -30,6 +42,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // routes setup
+app.use("/", indexRouter);
 app.use("/users", userRouter);
 app.use("/uploads", uploadRouter);
 
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
+const isAuthenticated = require("../middlewares/isAuth.middleware");
 const User = require("../models/User.model");
-const bcrypt = require("bcrypt");
 
-router.get("/", async (req, res) => {
+router.get("/", isAuthenticated, async (req, res) => {
   const users = await User.find({});
   if (!users || users.length === 0)
     return res.status(404).json({ message: "No users found" });
@@ -14,25 +14,6 @@ router.get("/:id", async (req, res) => {
   const user = await User.findById(userID).populate("uploads");
   if (!user) return res.status(404).json({ message: "User not found" });
   res.status(200).json(user);
-});
-
-router.post("/create-user", async (req, res) => {
-  const { username, password, fullName } = req.body;
-
-  // Check if the username already exists
-  const userExists = await User.findOne({ username: username });
-  if (userExists)
-    return res.status(400).json({ message: "Username already exists" });
-
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newUser = await User.create({
-    username,
-    password: hashedPassword,
-    fullName,
-  });
-  res.status(201).json(newUser);
 });
 
 module.exports = router;
